69bca4af10dee133aba6f330907d905f56978735
BupTy03 <41854435+BupTy03@users.noreply.github.com>
2018-07-31 10:38:39 +0100

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
+#include <string>
 #include "complex.hpp"
 
 using namespace std;
 
 int main()
 {
-	Complex<float> A{2.0, 4.2};
-	Complex<float> B{5.4};
+	constexpr Complex<float> A{2.0, 4.2};
+	constexpr Complex<float> B{5.4};
+	constexpr Complex<float> C;
 
-	cout << A + B << endl;
+	try{
+		cout << "A + B = " << A + B << endl;
+		cout << "A - B = " << A - B << endl;
+		cout << "A * B = " << A * B << endl;
+		cout << "A / B = " << A / B << endl;
+		cout << "A / C = " << A / C << endl;
+		cout << "A / 0 = " << A / 0.0 << endl;
+		cout << "arg(A) = " << to_degrees(arg(A)) << endl;
+		cout << "sqrt(A) = " << sqrt(A) << endl;
+		cout << "pow(A) = " << pow(A) << endl;
+		cout << "abs(A) = " << abs(A) << endl;
+	}
+	catch(Complex_error err){
+		cout << err.what() << endl;
+	}
 
 	return 0;
 }
